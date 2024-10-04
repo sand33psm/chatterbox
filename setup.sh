e7e93fe3c01d52622676486106b48f59051abec4
sand33psm <sand33psm@gmail.com>
2024-10-04 13:12:50 +0530

    Configure setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# install dependancies
+pip install setuptools
+pip install -r requirements.txt
+
+# Run Django commands
+python manage.py makemigrations
+python manage.py migrate
+python manage.py tailwind install
+python manage.py collectstatic
+python manage.py tailwind start
+
